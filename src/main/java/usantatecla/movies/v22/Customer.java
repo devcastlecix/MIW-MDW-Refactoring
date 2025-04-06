2bd185bcdc080d5f74ce9ffdb7c866964f162fe2
devcastlecix <luis_castle_2011@hotmail.com>
2025-04-06 01:30:12 +0100

    v22 - Eliminate duplication of the 'while' structure in several methods of the Customer class
    The “Extract Method” technique is used. By extracting the repeated loop and parameterizing the variable process, it can also be interpreted as a combination of “Introduce Parameter”

---
+++ src/main/java/usantatecla/movies/v22/Customer.java
@@ -0,0 +1,54 @@
+package usantatecla.movies.v22;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class Customer {
+
+	private String name;
+
+	private List<Rental> rentals;
+
+	public Customer(String name) {
+		this.name = name;
+		rentals = new ArrayList<Rental>();
+	}
+
+	public void addRental(Rental rental) {
+		rentals.add(rental);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String statement() {
+		String result = "Rental Record for " + this.getName() + "\n";
+
+		result = summarizeRentals(result, (currentText, rental) ->
+				currentText + "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n");
+
+		result += "Amount owed is " + String.valueOf(this.getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(this.getTotalFrequentRenterPoints()) + " frequent renter points";
+		return result;
+	}
+
+	private double getTotalCharge() {
+		return summarizeRentals(0.0, (total, rental) -> total + rental.getCharge());
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		return summarizeRentals(0, (total, rental) -> total + rental.getFrequentRenterPoints());
+	}
+
+	private <T> T summarizeRentals(T result, BiFunction<T, Rental, T> process) {
+		Iterator<Rental> rentals = this.rentals.iterator();
+		while (rentals.hasNext()) {
+			Rental each = rentals.next();
+			result = process.apply(result, each);
+		}
+		return result;
+	}
+}
